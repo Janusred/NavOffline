710dfb2bc049957f58686f5b8d6de19de9cffc61
Janusred <horaciok300@hotmail.com>
2022-10-17 04:26:02 -0500

    Correccion en codigo

--- sw.js
+++ sw.js
@@ -1,36 +1,21 @@
-// const CACHE_NAME = 'cache-1';
+//const CACHE_NAME = 'cache-1';
 const CACHE_STATIC_NAME = 'static-v2';
 const CACHE_DYNAMIC_NAME = 'dynamic-v1';
 const CACHE_INMUTABLE_NAME = 'inmutable-v1';
-
 const CACHE_DYNAMIC_LIMIT = 50;
-
-
 function limpiarCache(cacheName, numeroItems) {
-
-
     caches.open(cacheName)
         .then(cache => {
-
             return cache.keys()
                 .then(keys => {
-
                     if (keys.length > numeroItems) {
                         cache.delete(keys[0])
                             .then(limpiarCache(cacheName, numeroItems));
                     }
                 });
-
-
         });
 }
-
-
-
-
 self.addEventListener('install', e => {
-
-
     const cacheProm = caches.open(CACHE_STATIC_NAME)
         .then(cache => {
 
@@ -43,54 +28,27 @@ self.addEventListener('install', e => {
                 '/img/no-img.jpg',
                 '/pages/offline.html'
             ]);
-
-
         });
-
     const cacheInmutable = caches.open(CACHE_INMUTABLE_NAME)
         .then(cache => cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'));
-
-
     e.waitUntil(Promise.all([cacheProm, cacheInmutable]));
-
 });
-
 self.addEventListener('fetch', e => {
     const respuesta = caches.match(e.request)
         .then(res => {
-
             if (res) return res;
-
-            // No existe el archivo
-
-
-
-
             return fetch(e.request).then(newResp => {
-
                 caches.open(CACHE_DYNAMIC_NAME)
                     .then(cache => {
                         cache.put(e.request, newResp);
                         limpiarCache(CACHE_DYNAMIC_NAME, 50);
                     });
-
                 return newResp.clone();
             }).catch(err => {
                 if (e.request.headers.get('accept').includes('text/html')) {
                     return caches.match('/pages/offline.html');
                 }
-
-
-
-
             });
-
-
         });
-
-
-
-
     e.respondWith(respuesta);
-
 });
